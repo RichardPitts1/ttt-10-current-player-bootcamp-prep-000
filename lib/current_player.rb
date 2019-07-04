8376653bc794ca39e5f7d6212792ddacf04d4ab0
Richard Pitts <richardepitts@gmail.com>
2019-07-04 15:30:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,22 +1,22 @@
-def turn_count(turn)
-  moves = []
-  turn.each do |unit|
-    #counter = 0
-    if unit == "X" || unit == "O"
-      moves.push(1)
-    end 
-  end 
-  moves.length.to_i
-end 
+# def turn_count(turn)
+#   moves = []
+#   turn.each do |unit|
+#     #counter = 0
+#     if unit == "X" || unit == "O"
+#       moves.push(1)
+#     end 
+#   end 
+#   moves.length.to_i
+# end 
 
-board = ["X","X"," "," ",""," "," "," "," "]
+# board = ["X","X"," "," ",""," "," "," "," "]
 
-def current_player(board)
-  if turn_count(board) % == 0   
-  num = turn_count(board)
-  if num % 2 == 0  
-     return "X"
-    else 
-      return "0"   
-    end 
-  end 
+# def current_player(board)
+#   if turn_count(board) % == 0   
+#   num = turn_count(board)
+#   if num % 2 == 0  
+#     return "X"
+#     else 
+#       return "0"   
+#     end 
+#   end 
