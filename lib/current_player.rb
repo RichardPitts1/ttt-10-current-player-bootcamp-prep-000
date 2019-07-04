e835e05f6cd0dd27f1065da261d8f9aa7cf57f16
Richard Pitts <richardepitts@gmail.com>
2019-07-04 15:27:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,9 @@
 def turn_count(turn)
-  #counter = 1
   moves = []
   turn.each do |unit|
     #counter = 0
     if unit == "X" || unit == "O"
-
       moves.push(1)
-
     end 
   end 
   moves.length.to_i
