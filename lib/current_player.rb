385edc2151b47e5aa716d7c3b183c8daf5dc1360
Richard Pitts <richardepitts@gmail.com>
2019-07-04 15:23:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn_count(turn)
   #counter = 1
-  move_count = []
+  moves = []
   turn.each do |unit|
     #counter = 0
     if unit == "X" || unit == "O"
       #puts "spot taken"
-      move_count.push(1)
+      moves.push(1)
     #else 
       #puts "spot free"
       #counter += 1
