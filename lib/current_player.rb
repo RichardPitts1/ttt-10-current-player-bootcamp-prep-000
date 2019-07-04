00fa90a7bf71a5e4e409ce22337ed4d9feb98a49
Richard Pitts <richardepitts@gmail.com>
2019-07-04 15:30:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -20,3 +20,18 @@
 #       return "0"   
 #     end 
 #   end 
+
+def turn_count(board)
+   counter = 0
+   board.each do 
+spaces
+      if spaces == "X" 
+ spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
